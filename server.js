0de9997fdafe80b3dce31fde5191108fff2b0f43
collintegral <collintegral@gmail.com>
2025-03-08 09:00:38 -0500

    added basic routing for w01

---
+++ server.js
@@ -0,0 +1,10 @@
+import "./require.js";
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, () => { console.log(`Running on port ${port}`) });
