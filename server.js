06c8b9b4ce722475e519a59b80afe394831f56fe
collintegral <collintegral@gmail.com>
2025-03-08 09:55:58 -0500

    Basic routing

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import "./require.js";
 
 const express = require('express');
+const mongo = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => { console.log(`Running on port ${port}`) });
+mongo.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => { console.log(`Database is listening. Node running on port ${port}`) });
+    }
+});
