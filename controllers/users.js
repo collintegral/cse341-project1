3be596b93294f2bd64bfab7a6aad2c4929996719
collintegral <collintegral@gmail.com>
2025-03-13 18:09:13 -0400

    Created REST endpoints

--- controllers/users.js
+++ controllers/users.js
@@ -20,4 +20,50 @@ userController.getSingle = async (req, res) => {
     });
 }
 
+userController.createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+
+    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occurred while updating the user.')
+    }
+}
+
+userController.updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occurred while updating the user.')
+    }
+}
+
+userController.deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+
+    const response = await mongodb.getDatabase().db().collection('users').deleteOne({_id: userId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occurred while updating the user.')
+    }
+}
+
 module.exports = userController;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import "./require.js";
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongo = require('./data/database.js');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongo.initDb((err) => {
     if(err) {
